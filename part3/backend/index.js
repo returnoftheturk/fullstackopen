442cb6ff8105502978a823527541cddcdde83b87
Ahmet Akgul <ahmet.akgul@mail.mcgill.ca>
2024-01-01 18:04:30 -0500

    Commit

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -28,14 +28,8 @@ app.get("/api/notes", (request, response) => {
   })
 });
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body;
 
   const note = new Note({
     content: body.content,
@@ -44,17 +38,17 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote)
-  });
+  }).catch(next);
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-  Note.findByIdAndUpdate(request.params.id, note, { new: true})
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }  
+  )
     .then(updateNote => {
       response.json(updateNote)
     })
@@ -89,7 +83,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
