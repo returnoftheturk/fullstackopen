46afa1829efcf09b0dd706a65674a9cc05ff892c
Ahmet Akgul <ahmet.akgul@mail.mcgill.ca>
2024-03-03 17:20:59 -0500

    Multiple reducers

--- part6/simplifiedNoteApplication/src/main.jsx
+++ part6/simplifiedNoteApplication/src/main.jsx
@@ -1,10 +1,21 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux'
-import noteReducer from './reducers/noteReducer';
+import noteReducer, {createNote} from './reducers/noteReducer';
+import filterReducer, {filterChange} from './reducers/filterReducer';
 import App from './App';
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+console.log(store.getState())
+
+store.subscribe(() => console.log('subscribe log:', store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
