550670fd28173122a8cef6bf5f3e4300da1c683e
Ahmet Akgul <ahmet.akgul@mail.mcgill.ca>
2024-03-10 17:48:00 -0400

    Redux thunk

--- part6/simplifiedNoteApplication/src/main.jsx
+++ part6/simplifiedNoteApplication/src/main.jsx
@@ -1,29 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-import noteReducer from './reducers/noteReducer';
-import filterReducer, {filterChange} from './reducers/filterReducer';
+import store from './store';
 import App from './App';
 
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-store.subscribe(() => console.log('subscribe log:', store.getState()))
-store.dispatch(filterChange('ALL'))
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  )
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
--- part6/simplifiedNoteApplication/src/reducers/noteReducer.js
+++ part6/simplifiedNoteApplication/src/reducers/noteReducer.js
@@ -1,10 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
+    appendNote(state, action) {
       state.push(action.payload)
     },
     toggleImportanceOf(state, action) {
@@ -14,14 +15,25 @@ const noteSlice = createSlice({
         note.id !== changedNote.id ? note : changedNote 
       )     
     },
-    appendNote(state, action) {
-      state.push(action.payload)
-    },
     setNotes(state, action) {
       return action.payload
     }
   },
 })
 
-export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+export const { appendNote, toggleImportanceOf, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
 export default noteSlice.reducer
