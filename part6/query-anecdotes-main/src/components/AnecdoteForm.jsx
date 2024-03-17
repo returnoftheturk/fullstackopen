3243675045549f2de0313c69b34cab62a7c98c47
Ahmet Akgul <ahmet.akgul@mail.mcgill.ca>
2024-03-17 14:55:39 -0400

    Add notification when error

--- part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -1,13 +1,21 @@
 import { useQueryClient, useMutation } from '@tanstack/react-query'
 import { createAnecdote } from '../requests'
+import { useNotificationDispatch } from '../NotificationContext'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const dispatchNotification = useNotificationDispatch();
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    },
+    onError: (err) => {
+      dispatchNotification({type: 'SET_NOTIFICATION', payload: err?.response?.data?.error})
+      setTimeout(() => {
+        dispatchNotification({type: 'CLEAR_NOTIFICATION'})
+      }, 5000)
     }
   })
 
